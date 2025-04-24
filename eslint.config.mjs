23c0dcedee1ade59bb11c09e2be6d6593f03c518
MarkDanilchenko <danilchenko.mark50@gmail.com>
2025-04-25 00:04:17 +0300

    Configuration of eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,63 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginVue from "eslint-plugin-vue";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import eslintPluginJest from "eslint-plugin-jest";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,vue}", "**/*.test.{js,mjs,cjs}"],
+    ignores: ["**/node_modules/**", "**/dist/**", "**/lib/**", "**/build/**", "**/coverage/**", "**/public/**"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jquery,
+        ...globals.es2021,
+        ...eslintPluginJest.environments.globals.globals,
+      },
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      js,
+      vue: pluginVue.configs["flat/recommended"],
+      jest: eslintPluginJest,
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      "prettier/prettier": ["error", { doubleQuote: true, printWidth: 120 }],
+      ...js.configs.recommended.rules,
+      ...eslintPluginJest.configs.recommended.rules,
+      ...pluginVue.configs["flat/recommended"].rules,
+      "vue/attribute-hyphenation": "warn",
+      "vue/v-on-event-hyphenation": "warn",
+      "vue/multi-word-component-names": "off",
+      "vue/component-tags-order": [
+        "error",
+        {
+          order: ["template", "script", "style"],
+        },
+      ],
+      "prefer-const": "error",
+      "no-var": "error",
+      "no-console": "error",
+      "no-unused-expressions": "error",
+      "no-unused-vars": "error",
+      "no-use-before-define": "error",
+      "no-useless-constructor": "error",
+      semi: ["error", "always"],
+      "padding-line-between-statements": [
+        "error",
+        {
+          blankLine: "always",
+          prev: "*",
+          next: "return",
+        },
+      ],
+    },
+  },
+]);

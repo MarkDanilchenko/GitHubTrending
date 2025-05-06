b90717dad40409ea36619379bfee9dbc07e0bfc5
MarkDanilchenko <danilchenko.mark50@gmail.com>
2025-05-07 00:13:02 +0300

    Edit clock and main vue file

---
+++ client/src/components/Clock.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="my-clock">
+    <div class="custom-clock">{{ clock }}</div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Clock",
+  data() {
+    return {
+      clock: null,
+    };
+  },
+  methods: {
+    timer() {
+      this.clock = new Date().toLocaleTimeString();
+      setTimeout(this.timer, 1000);
+    },
+  },
+  mounted() {
+    this.timer();
+  },
+  beforeUnmount() {
+    clearTimeout(this.timer);
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>

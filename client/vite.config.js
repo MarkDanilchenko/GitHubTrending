2fc4e1cc40236f27567ae9bb650ab389b6ef147b
MarkDanilchenko <danilchenko.mark50@gmail.com>
2025-05-09 23:13:03 +0300

    Edit envs

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,23 @@
-import { fileURLToPath, URL } from "node:url";
 import { defineConfig, loadEnv } from "vite";
+import { fileURLToPath, URL } from "node:url";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
+import dotenv from "dotenv";
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, `./.env.development`, "");
+  const env = loadEnv(mode, process.cwd(), "");
+  const externalEnv =
+    dotenv.config({
+      path: "./.env.development",
+    }) || {};
+  const combinedEnv = { ...env, ...externalEnv };
+  const clientEnv = Object.keys(combinedEnv).reduce((acc, key) => {
+    if (key.startsWith("VITE_")) {
+      acc[key] = combinedEnv[key];
+    }
+
+    return acc;
+  }, {});
 
   return {
     plugins: [vue(), vueDevTools()],
@@ -14,7 +27,7 @@ export default defineConfig(({ mode }) => {
       },
     },
     define: {
-      "process.env": env,
+      "process.env": clientEnv,
     },
   };
 });
